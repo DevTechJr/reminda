cc3505c53badbf6d8ff344499aa7f285ef88c629
DevTechJr <devtechjr@gmail.com>
2024-08-18 02:51:38 -0400

    dashboard styling done

--- website/views.py
+++ website/views.py
@@ -49,9 +49,32 @@ def home():
     for sighting in sightings:
         timestamps.append(sighting.time)
         sightingData.append({"name": sighting.name, "location": sighting.location, "time": sighting.time})
-    
 
-    return render_template("home.html", user=current_user, sightings=sightings, timestamps=timestamps)
+    people = People.query.all()
+    peopleData = []
+
+    # Create a dictionary to store the last seen timestamp for each person
+    last_seen = {}
+    for sighting in sightings:
+        if sighting.name not in last_seen:
+            last_seen[sighting.name] = sighting.time
+        else:
+            # Update the last seen timestamp if the current one is more recent
+            if sighting.time > last_seen[sighting.name]:
+                last_seen[sighting.name] = sighting.time
+
+    # Construct peopleData with last_seen_at
+    for person in people:
+        peopleData.append({
+            "name": person.name,
+            "picture": person.picture,
+            "relation": person.relation,
+            "summary": person.summary,
+            "specialMemory": person.specialMemory,
+            "last_seen_at": last_seen.get(person.name, None)  # Get the last seen timestamp or None if not found
+        })
+    
+    return render_template("home.html", user=current_user, sightings=sightings, timestamps=timestamps, sightingData=sightingData, peopleData=peopleData)
 
 @views.route('/addSighting', methods=['POST', "GET"])
 def addSighting():
